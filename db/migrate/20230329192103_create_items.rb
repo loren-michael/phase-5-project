e7a5f0871e6b35fe168318dfe3fe6cfcb1ac7e29
loren <lorenmichaelh@gmail.com>
2023-03-29 16:03:14 -0500

    rails: editing item migration
    BREAKING CHANGE: price column type incorrect, going to try money gem to make pricing formats easy

--- db/migrate/20230329192103_create_items.rb
+++ db/migrate/20230329192103_create_items.rb
@@ -2,11 +2,15 @@ class CreateItems < ActiveRecord::Migration[7.0]
   def change
     create_table :items do |t|
       t.belongs_to :user_id, null: false, foreign_key: true
-      t.string :name
+      t.string :title
       t.string :image
       t.string :description
       t.integer :qty
       t.string :category
+      t.string :condition
+      t.string :make
+      t.string :model
+      t.price :price
 
       t.timestamps
     end

5d5cd7de0d64f117f94774ca45ec271f44fb46f5
loren <lorenmichaelh@gmail.com>
2023-04-07 16:48:20 -0500

    feat: another redux toolkit installation

--- client/src/index.js
+++ client/src/index.js
@@ -1,17 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { store } from './app/store';
 import App from './App';
-import rootReducer from './reducers';
 import reportWebVitals from './reportWebVitals';
 
-const store = createStore(rootReducer);
+// const store = configureStore(rootReducer);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Provider store={ store }>
+    <Provider store ={ store }>
       <App />
     </Provider>
   </React.StrictMode>

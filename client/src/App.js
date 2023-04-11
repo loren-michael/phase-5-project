5bfbed4a966e53e678f22a6a828b83c818a82445
loren <lorenmichaelh@gmail.com>
2023-04-11 17:54:11 -0500

    chore: add routes

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,13 @@ import React, { useEffect } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import { useSelector, useDispatch } from 'react-redux';
-
+import Home from './components/Home';
+import StoresContainer from './components/StoresContainer';
+import Store from './components/Store';
+import Cart from './components/Cart';
+import ItemsContainer from './components/ItemsContainer';
+import ItemDetails from './components/ItemDetails';
+import Profile from './components/Profile';
 import NavBar from './components/NavBar'
 
 function App() {
@@ -21,7 +27,13 @@ function App() {
       <div>
         <NavBar items={items}  />
         <Routes>
-          <Route exact path="/" />
+          <Route exact path="/" element={ <Home /> }/>
+          <Route path="/stores" element={ <StoresContainer /> } />
+          <Route path="/stores/:id" element={ <Store /> } />
+          <Route path="/cart" element={ <Cart /> } />
+          <Route path="/items" element={ <ItemsContainer /> } />
+          <Route path="/items/:id" element={ <ItemDetails /> } />
+          <Route path="/user" element={ <Profile /> } />
         </Routes>
       </div>
     </BrowserRouter>

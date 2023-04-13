8a9ba5c4b42d6bab578861dec0c9877d7f6abfb3
loren <lorenmichaelh@gmail.com>
2023-04-13 11:34:48 -0500

    chore: build login and sign up components
    login and sign up components need to be linked to redux

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,8 +1,60 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Login = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [errors, setErrors] = useState([])
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    fetch('./login', {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      },
+      body: JSON.stringify({
+        username: username,
+        password: password
+      })
+    })
+    .then(r => {
+      if (r.ok) {
+        r.json().then(user => console.log(user))
+      } else {
+        r.json().then(data => console.log(data))
+      }
+    })
+  }
+
   return (
-    <div>Login</div>
+    <div>
+      <h3>Log In</h3>
+      <form onSubmit={handleLogin}>
+        <label htmlFor='username'>Username: </label>
+        <input
+          type="text"
+          value={username}
+          onChange={e => setUsername(e.target.value)}
+        />
+        <label htmlFor='password'>Password: </label>
+        <input 
+          type="password"
+          value={password}
+          onChange={e => setPassword(e.target.value)}
+        />
+        <input type="submit" value="Log In" />
+      </form>
+      <br></br>
+      <br></br>
+      <ul>
+        {errors.map(err => {
+          return (
+            <li key={err}>{err}</li>
+          )
+        })}
+      </ul>
+    </div>
   )
 }
 
--- client/src/components/SignUp.js
+++ client/src/components/SignUp.js
@@ -1,8 +1,60 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const SignUp = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [errors, setErrors] = useState([]);
+
+  const handleSignup = (e) => {
+    e.preventDefault();
+    fetch('/signup', {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      },
+      body: JSON.stringify({
+        username: username,
+        password: password
+      })
+    })
+    .then(r => {
+      if (r.ok) {
+        r.json().then(user => console.log(user))
+      } else {
+        r.json().then(data => console.log(data))
+      }
+    })
+  }
+
   return (
-    <div>SignUp</div>
+    <div>
+      <h3>Sign Up</h3>
+      <form onSubmit={ handleSignup } >
+      <label htmlFor='username'>Username: </label>
+      <input 
+        type="text"
+        value={ username }
+        onChange={ e => setUsername(e.target.value) }
+      />
+      <label htmlFor='password'>Password: </label>
+      <input
+        type="password"
+        value={ password }
+        onChange={e => setPassword(e.target.value)}
+      />
+      <input type="submit" value="Sign Up" />
+      </form>
+      <br></br>
+      <br></br>
+      <ul>
+        {errors.map(err => {
+          return (
+            <li key={err}>{err}</li>
+          )
+        })}
+      </ul>
+    </div>
   )
 }
 

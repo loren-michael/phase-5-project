db2906e2a2439823f097935c1a558bac899fe4c0
Loren Hartman <lorenmichaelh@gmail.com>
2023-04-19 11:49:35 -0500

    chore: moved images to src dir, add logo to navbar and adjust colors

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faS, faTimes, faInfoCircle, faChampagneGlasses } from '@fortawesome/free-solid-svg-icons';
-<FontAwesomeIcon icon="fa-solid fa-s" />
+import logo from '../images/sellit-4.png'
 
 const NavBar = () => {
 
   return (
     <div className="navBar">
       <span>
-        <FontAwesomeIcon icon={faS}/>
+        <img src={logo} alt="Sell It!" width="300" height="auto"></img>
       </span>
     </div>
   )

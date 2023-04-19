b798fd9ff0e99634a91f4ddb41ec18c6f3b59fcd
Loren Hartman <lorenmichaelh@gmail.com>
2023-04-19 11:12:52 -0500

    feat: add images dir with icon files for navbar

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faS, faTimes, faInfoCircle, faChampagneGlasses } from '@fortawesome/free-solid-svg-icons';
+<FontAwesomeIcon icon="fa-solid fa-s" />
 
 const NavBar = () => {
 
   return (
-    <div>NavBar
+    <div className="navBar">
+      <span>
+        <FontAwesomeIcon icon={faS}/>
+      </span>
     </div>
   )
 }

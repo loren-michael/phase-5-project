cf1aaaea39869de5dde6a3ffd0bc30e3371a2d40
loren <lorenmichaelh@gmail.com>
2023-04-25 17:10:39 -0500

    feat: added application controller code

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,20 @@
 class ApplicationController < ActionController::API
   include ActionController::Cookies
+  rescue_from ActiveRecord::RecordInvalid, with: :render_422
+  before_action :authorize
 
-  def hello_world
-    session[:count] = (session[:count] || 0) + 1
-    render json: {count: session[:count]}
-  end
+
+
+  private
+
+    def authorize
+      @current_user = User.find_by(id: session[:user_id])
+      render json: { errors: ["User not authorized"] },
+      status: :unauthorized unless @current_user
+    end
+
+    def render_422(exception)
+      render json: { errors: exception.redord.errors.full_messages }, status: 422
+    end
+
 end
--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -15,13 +15,27 @@ class ItemsController < ApplicationController
 
   # POST /items
   def create
-    @item = Item.new(item_params)
+    # @item = Item.new(item_params)
 
-    if @item.save
-      render json: @item, status: :created, location: @item
-    else
-      render json: @item.errors, status: :unprocessable_entity
-    end
+    # if @item.save
+    #   render json: @item, status: :created, location: @item
+    # else
+    #   render json: @item.errors, status: :unprocessable_entity
+    # end
+    item = @current_user.items.create!(
+      user_id: @current_user.id,
+      title: params[:title],
+      image: params[:image],
+      decsription: params[:description],
+      qty: params[:qty],
+      category: params[:category],
+      condition: params[:condition],
+      make: params[:make],
+      model: params[:model],
+      price: params[:price]
+    )
+
+
   end
 
   # PATCH/PUT /items/1
@@ -46,6 +60,6 @@ class ItemsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def item_params
-      params.require(:item).permit(:user_id, :name, :image, :description, :qty, :category)
+      params.permit(:user_id, :title, :image, :description, :qty, :category, :condition, :make, :model, :price)
     end
 end

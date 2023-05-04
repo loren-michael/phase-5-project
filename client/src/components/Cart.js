8ce308780b28bae2e6f092f1b13475ece1dfe268
loren <lorenmichaelh@gmail.com>
2023-05-04 18:15:51 -0500

    feat: add more styling to cart component

--- client/src/components/Cart.js
+++ client/src/components/Cart.js
@@ -23,16 +23,19 @@ const Cart = () => {
         <h3 class="pl-20">Your Carts:</h3>
         {carts.carts.map(cart => {
           return (
-            <div key={cart.id} class="flex flex-wrap w-9/12 h-24 m-auto bg-slate-200 rounded border-2 border-black ">
-              <div class="w-24">{cart.active ? <FontAwesomeIcon icon={faCartShopping} size="2xl" style={{color: "#00ff00",}} /> : <FontAwesomeIcon icon={faCartShopping} size="2xl" style={{color: "#ff0000",}} />}</div>
-              <div>Items:
+            <div key={cart.id} class="flex flex-wrap w-9/12 m-auto bg-slate-200 rounded border-2 border-black ">
+              <div class="w-24">{cart.active ? <div class="m-8"><FontAwesomeIcon icon={faCartShopping} size="2xl" style={{color: "#00ff00",}} /></div> : <div class="m-8"><FontAwesomeIcon icon={faCartShopping} size="2xl" style={{color: "#ff0000",}} /></div>}</div>
+              <div class="pl-6 w-2/4">Items:
                 {cart.items.map(item => {
                   return (
-                    <li>{item.title.substring(0,20)}</li>
+                    <div class="flex flex-wrap">
+                      <li>{item.title.substring(0,20)}</li>
+                      <div class="pl-12"> ${item.price}</div>
+                    </div>
                   )
                 })}
               </div>
-              <div>Hold space for price</div>
+              <div class="justify">{  }</div>
             </div>
           )
         })}
